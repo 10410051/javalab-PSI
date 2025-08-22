1f7d48e55930c83823f2112812f4a4eaed6aced9
10410051 <226614468+10410051@users.noreply.github.com>
2025-08-21 22:26:03 -0300

    Exer1

---
+++ aula02/ExerAula02/Multa.java
@@ -0,0 +1,27 @@
+package aula02.ExerAula02;
+import java.util.Scanner;
+
+public class Multa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Livro: \n");
+        String livro = sc.nextLine();
+        System.out.printf("Dias: \n");
+        int atrasoDias = sc.nextInt();
+
+        double multa = atrasoDias * 1.25;
+
+        String linha = "+" + "-".repeat(20) + "+";
+        System.out.println("AVISO DE MULTA - BIBLIOTECA");
+        System.out.println(linha);
+        System.out.printf("Livro: %s\n", livro);
+        System.out.printf("Dias em Atraso: %d\n", atrasoDias);
+        System.out.printf("Multa Total: %d\n", multa);
+        System.out.println(linha);
+        System.out.println("por favor, regularize sua situação");
+
+        sc.close();
+        
+    }
+}
