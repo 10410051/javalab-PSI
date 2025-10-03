9813eec7405947f0bfcb7218f48a983a281bec69
10410051 <226614468+10410051@users.noreply.github.com>
2025-10-02 22:37:19 -0300

    Começo Aula05

---
+++ JavaLab/aula05/Aluno.java
@@ -0,0 +1,13 @@
+package JavaLab.aula05;
+
+public class Aluno {
+    // atributos
+    private String nome;
+    private double n1;
+    private double n2;
+
+    // construtor
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ JavaLab/aula05/TesteAluno.java
@@ -0,0 +1,8 @@
+package JavaLab.aula05;
+
+public class TesteAluno {
+    public static void main(String[] args) {
+        Aluno a = new Aluno("Enzo");
+
+    }
+}
