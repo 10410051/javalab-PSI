7ff7c3e8d1bd620991462431b1c0a1e8e434e3d3
10410051 <226614468+10410051@users.noreply.github.com>
2025-10-09 22:22:47 -0300

    Add Circulo.java

---
+++ JavaLab/aula08/Circulo.java
@@ -0,0 +1,19 @@
+package JavaLab.aula08;
+
+public class Circulo {
+    private double raio;
+    private Ponto centro;
+
+    public Circulo (Ponto centro, double raio) {
+        this.centro = centro;
+        this.raio = raio;
+    }
+
+    public boolean estaContido(Ponto p) {
+        double distancia = centro.distancia(p);
+        return distancia <= raio;
+    }
+    
+    
+
+}
--- JavaLab/aula08/Main.java
+++ JavaLab/aula08/Main.java
@@ -5,6 +5,9 @@ public class Main {
         Ponto p1 = new Ponto(1, 1);
         Ponto p2 = new Ponto(3, 3);
         double distancia = p1.distancia(p2);
+        Circulo c = new Circulo(p1, 2);
+        boolean estaPerto = c.estaContido(p2);
+        System.out.printf("Esta perto: %b\n", estaPerto);
         System.out.printf("Distancia: %.2f\n", distancia);
     }
 }
